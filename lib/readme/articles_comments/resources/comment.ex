72c58a8d4033d7b9fc2115dbe81354bce765a242
Sahid Rahman <sahidrahman404@gmail.com>
2023-02-11 17:48:17 +0700

    change comment resources

--- lib/readme/articles_comments/resources/comment.ex
+++ lib/readme/articles_comments/resources/comment.ex
@@ -27,47 +27,75 @@ defmodule Readme.ArticlesComments.Comment do
     table "articles_comments"
     repo Readme.Repo
 
-    migration_ignore_attributes [
-      :id,
-      :created_at,
-      :updated_at,
-      :content,
-      :path,
-      :parent_path
-    ]
+    migration_ignore_attributes [:path, :parent_path]
 
     custom_statements do
-      statement :articles_comments_table do
+      statement :add_path_and_parent_path_column do
         up """
-        create table articles_comments (
-        -- primary key
-        id bigserial primary key,
-
-        -- surrogate key
-        path ltree generated always as (coalesce(parent_path::text,'')::ltree || id::text::ltree) stored unique,
-
-        -- foreign key
-        parent_path ltree, 
-        constraint fk_parent
-        foreign key(parent_path) 
-        references articles_comments(path)
-        on delete cascade
-        on update cascade,
-
-        -- content
-        content text,
-
-        created_at timestamptz default current_timestamp,
-        updated_at timestamptz default current_timestamp,
-
-        accounts_id bigint references accounts(id) on delete cascade
-        );
+          alter table articles_comments
+          add column parent_path ltree, 
+          add column path ltree generated always as (coalesce(parent_path::text,'')::ltree || id::text::ltree) stored unique;
         """
 
         down """
-          drop table articles_comments;
+          alter table articles_comments
+          drop column path, 
+          drop column parent_path;
         """
       end
+
+      statement :add_constraint_parent_path do
+        up """
+        alter table articles_comments
+        add constraint fk_parent
+        foreign key(parent_path)
+        references articles_comments(path)
+        on delete cascade
+        on update cascade;
+        """
+
+        down """
+        alter table articles_comments
+        drop constraint fk_parent;
+        """
+      end
+
+      statement :add_index_accounts_id do
+        up """
+        create index articles_comments_account_id_idx on articles_comments(accounts_id);
+        """
+
+        down """
+        drop index articles_comments_account_id_idx;
+        """
+      end
+
+      statement :add_index_parent_path do
+        up """
+        create index articles_comments_parent_path_idx on articles_comments(parent_path asc nulls first);
+        """
+
+        down """
+        drop index articles_comments_parent_path_idx;
+        """
+      end
+
+      statement :add_index_path do
+        up """
+        create index articles_comments_path_idx on articles_comments using gist (path);;
+        """
+
+        down """
+        drop index articles_comments_path_idx;
+        """
+      end
+    end
+  end
+
+  relationships do
+    belongs_to :accounts, Readme.Accounts.Account do
+      api Readme.Accounts
+      attribute_type(:integer)
     end
   end
 end
